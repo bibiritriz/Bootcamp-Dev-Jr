f664f71ca208e5c8ef555cd8f3b684dad6236a91
Beatriz Camargo <ntenhodoritosmasdepende@gmail.com>
2024-07-26 18:30:38 -0300

    closes #19, closes #20 - Historico

--- Modulo2/Cap 1/projeto-calculadora/javascript.js
+++ Modulo2/Cap 1/projeto-calculadora/javascript.js
@@ -3,15 +3,21 @@ function calc(op){
   var num2 = parseFloat(document.getElementById("num2").value); 
   var saida = 0;
 
-  if(op === 'soma'){
-    var saida = num1 + num2;
-  }else if(op === 'sub'){
-    var saida = num1 - num2;
-  }else if(op === 'mult'){
-    var saida = num1 * num2;
-  }else{
-    var saida = num1 / num2;
+  switch(op){
+    case '+' : var saida = num1 + num2; break;
+    case '-' : var saida = num1 - num2; break;
+    case 'x' : var saida = num1 * num2; break;
+    case '/' : var saida = num1 / num2; break;
   }
 
   document.getElementById("saida").value = saida;
+
+  var novoHistorico = "<p>" + num1 + " " + op + " " + num2 + " " + "=" + " " + saida + "</p>";
+  var historico = document.getElementById("historico");
+
+  historico.innerHTML = novoHistorico + historico.innerHTML;
+
+  if(historico.children.length > 10){
+    historico.removeChild(historico.childNodes[10])
+  }
 }
