e5a9e460ced74601ca6f903661a33184bc6d57c4
Beatriz Camargo <ntenhodoritosmasdepende@gmail.com>
2024-07-26 16:47:32 -0300

    closes #18 - 4 operacoes matematicas

--- Modulo2/Cap 1/projeto-calculadora/javascript.js
+++ Modulo2/Cap 1/projeto-calculadora/javascript.js
@@ -1,8 +1,17 @@
-function calc(){
+function calc(op){
   var num1 = parseFloat(document.getElementById("num1").value);
   var num2 = parseFloat(document.getElementById("num2").value); 
+  var saida = 0;
 
-  var saida = num1 + num2;
+  if(op === 'soma'){
+    var saida = num1 + num2;
+  }else if(op === 'sub'){
+    var saida = num1 - num2;
+  }else if(op === 'mult'){
+    var saida = num1 * num2;
+  }else{
+    var saida = num1 / num2;
+  }
 
   document.getElementById("saida").value = saida;
 }
