4169df40f53a5e04689a84fdf3c9bcac12c238fb
Beatriz Camargo <ntenhodoritosmasdepende@gmail.com>
2024-08-04 18:05:14 -0300

    closes #45 - Historico

--- Modulo2/Cap4/projeto-profile-github/js/script.js
+++ Modulo2/Cap4/projeto-profile-github/js/script.js
@@ -1,14 +1,40 @@
+var UsersHistory = [];
+
 function search() {
   var username = document.getElementById("InputUserName").value;
   var url = `https://api.github.com/users/${username}`;
 
   $.getJSON(url, (user) => {
     showUserData(user);
+    if(isNew(user)){
+      save(user);
+      showNewUserHistory(user);
+    }
     clearError();
   }).fail(() => {
     showUserData({});
     showError("Usuário não encontrado!");
   });
+}
+
+function save(user){
+  UsersHistory.push(user);
+}
+
+function isNew(user){
+  return UsersHistory.filter((u) => u.login === user.login).length == 0;
+}
+
+function showNewUserHistory(user){
+  document.getElementById("history"). innerHTML += `<div class="col">
+              <img
+                src=${user.avatar_url}
+                class="mt-2 shadow rounded"
+                width="10%"
+                alt="avatar"
+                id="avatar_url"
+              />
+            </div>`
 }
 
 function showError(msg){
